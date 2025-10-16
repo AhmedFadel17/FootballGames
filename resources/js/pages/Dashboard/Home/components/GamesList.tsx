42e5830d0831aa5c6f2d98b4476616670bfa42ad
AhmedFadel17 <ahmedfadel1717@gmail.com>
2025-10-16 11:43:11 +0300

    Update GamesList.tsx

--- resources/js/pages/Dashboard/Home/components/GamesList.tsx
+++ resources/js/pages/Dashboard/Home/components/GamesList.tsx
@@ -1,5 +1,4 @@
 import { useGetDataQuery } from "@/services/api";
-import { Game, GameType } from "@/types/game";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -15,26 +14,36 @@ export default function GamesList() {
         }
     }, [isSuccess, data]);
     return (
-        <div className="">
+      <div className="flex items-center justify-center">
+
+        <div className="w-full md:w-10/12 px-6 md:px-0 space-y-10 py-10">
             {gamesList && gamesList.map(({ id, name, slug, description, games }: GameType, index) => (
-                <div key={index} className="py-10">
-                    <p className="text-xl font-[700]">{name}</p>
-                    <p>{description}</p>
-                    <div className="grid grid-cols-4 gap-4">
-                        {games && games.map(({ id, title, description, is_active }: Game, idx) => (
+                <div key={index} className="bg-white rounded border-2 border-primary">
+                    <div className="bg-primary text-white px-4 py-1">
+                        <p className="text-xl font-[700]">{name}</p>
+                        <p className="text-secondary">{description}</p>
+                    </div>
+
+                    <div className="grid grid-cols-3 gap-4 py-10 px-4">
+                        {games && games.map(({ id, title, description, is_active }: Game, idx: number) => (
                             <Link
                                 to={"/games/" + slug}
                             >
-                                <div key={idx} className="col rounded p-4 border-2">
-                                    <p className="text-lg font-[600]">{title}</p>
-                                    <p>{description}</p>
+                                <div key={idx} className="col rounded-lg bg-gradient-to-r from-[#ffc3a0] to-[#FFAFBD] text-primary min-h-48">
+                                    <div className="bg-secondary  px-4 py-2">
+                                        <p className="text-lg font-[600]">{title}</p>
+                                    </div>
+                                    <div className="p-4">
+                                        <p>{description}</p>
+
+                                    </div>
                                 </div>
                             </Link>
                         ))}
                     </div>
                 </div>
             ))}
-
+</div>
         </div>
     );
 }
